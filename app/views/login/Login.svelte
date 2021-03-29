65584af3bfedc6688cce7881a0562172497e8842
Ivan S <ivan.leondager@gmail.com>
2021-03-29 06:22:01 -0500

    adding loggin page

---
+++ app/views/login/Login.svelte
@@ -0,0 +1,64 @@
+<page {...pageProps}>
+    <flexboxLayout {...flexboxLayoutProps}>
+        <label horizontalAlignment="center" textWrap="true" verticalAlignment="middle">
+            <formattedString>
+                <span class="fas t-18" color="#63371E" text="Maco"/>
+            </formattedString>
+        </label>
+        <image {...imageProps} tintColor="{leafTintColor}"></image>
+        <PhoneTextField setCode={setCode} setPhone={setPhone}/>
+
+        <button {...buttonProps}/>
+    </flexboxLayout>
+</page>
+
+<script lang="typescript">
+    import PhoneTextField from "./phoneNumberTextField/PhoneTextField.svelte"
+
+    const pageProps: { [id: string]: string; } = {actionBarHidden: "true"}
+    const flexboxLayoutProps = {
+        backgroundColor: "#95C22B",
+        flexDirection: "column", justifyContent: "space-around", alignItems: "center"
+    }
+    const buttonProps = {
+        borderRadius: "5",
+        backgroundColor: "#63371E",
+        class: "my-button",
+        color: "#A7D930",
+        text: "Registrarme",
+        height: 50,
+        width: 300
+    }
+    const imageProps = {
+        height: "80",
+        src: "~/icons/seedling-solid.png",
+    }
+
+    let leafTintColor = "#507B00"
+    let code: string = "+57"
+    let phone: string = ""
+
+    const setCode = (newCode) => {
+        code = newCode
+    }
+    const setPhone = (newPhone) => {
+        phone = newPhone
+    }
+</script>
+
+<style>
+
+    .fa_custom {
+        color: #0099CC
+    }
+
+    .fas {
+        font-family: "Font Awesome 5 Free", "fa-solid-900";
+        font-weight: 2000;
+    }
+
+    .t-18 {
+        font-size: 120;
+    }
+</style>
+
---
+++ app/views/login/phoneNumberTextField/PhoneTextField.svelte
@@ -0,0 +1,70 @@
+<stackLayout>
+    <flexboxLayout {...flexboxLayoutProps}>
+        <gridLayout columns="180, 150" rows="50">
+            <label horizontalAlignment="center" textWrap="true" verticalAlignment="middle">
+                <formattedString>
+                    <span {...enterTextProps}/>
+                </formattedString>
+            </label>
+        </gridLayout>
+    </flexboxLayout>
+    <flexboxLayout {...flexboxLayoutProps}>
+        <gridLayout columns="120, 210" rows="100">
+            <listPicker {...listPickerProps} class="picker" on:selectedIndexChange={(val)=>{
+                setCode(val["value"])}}/>
+            <textField {...textFieldProps} class="textField" on:textChange={(val)=>{
+                setPhone(val["value"])
+            }}></textField>
+
+        </gridLayout>
+    </flexboxLayout>
+</stackLayout>
+
+<script>
+    import countryCodeList from "../CountryCodes.ts"
+    import {action} from '@nativescript/core/ui/dialogs'
+
+    export let setPhone;
+    export let setCode;
+
+    //components props
+    const textFieldProps = {col: "1", row: "0", text: "", hint: "...", keyboardType: "phone"}
+    const listPickerProps = {
+        col: "0", row: "0",
+        items: countryCodeList.map(codeItem => {
+            return codeItem["name"]
+        }),
+    }
+    const enterTextProps = {style: "color: black", class: "fas t-18", text: "Ingresa tu numero de telefono: "}
+    const flexboxLayoutProps = {alignItems: "center", justifyContent: "space-around"}
+</script>
+
+<style>
+    .picker {
+        background-color: #63371E;
+        color: #A7D930;
+        border-radius: 5;
+        width: 200;
+        height: 50;
+        vertical-align: baseline;
+    }
+
+    .textField {
+        justify-content: "center";
+        background-color: #63371E;
+        color: #95C22B;
+        border-radius: 5;
+        width: 200;
+        height: 50;
+        vertical-align: baseline;
+    }
+
+    .fas {
+        font-family: "Font Awesome 5 Free", "fa-solid-900";
+        font-weight: 200;
+    }
+
+    .t-18 {
+        font-size: 20;
+    }
+</style>
